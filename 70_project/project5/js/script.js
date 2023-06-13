16984a00c643edbefb0a3b9590cee016bb402b1d
moonsunho <tkdlqj0411@naver.com>
2023-06-13 17:51:41 +0900

    0613

--- 70_project/project5/js/script.js
+++ 70_project/project5/js/script.js
@@ -17,16 +17,75 @@ $(function () {
     // $('video').get(0).play();
 
     // 섹션1 케로셀
-    $(document).ready(function () {
-        $('.slider').bxSlider({
-            // 자동 실행
-            auto: true,
-            stopAutoOnClick: true,
-            // 일시정지와 재실행 버튼
-            // autoControls : true,
-            // pager: false,
-            // slideWidth: 1400
-        });
+    const slider = $('.slider').bxSlider({
+        mode: 'horizontal',
+        // mode: 'vertical',
+        // mode: 'fade',
+        // 자동 슬라이드
+        auto: true,
+        // 컨트롤 버튼(좌우, 페이저)를 클릭하면 auto 슬라이드 일시 정지
+        stopAutoOnClick: true,
+        // 슬라이드 위에 hover하면 auto 일시정지
+        autoHover: true,
+        // 실행/일시정지 버튼
+        // autoControls: true,
+        // 인디케이터 버튼
+        pager: false,
+        // 이전 이후 버튼
+        controls: false,
+        // 전환 시간
+        speed: 400,
+        // 지연 시간
+        pause: 3000,
+        // 슬라이드 전환 직전에 autoPager() 함수를 호출하여 동작 시킴
+        onSlideBefore: function () {
+            autoPager();
+        },
+        // 슬라이드 전환 직후 txtMotion() 함수를 호출하여 동작 시킴
+        onSlideAfter: function () {
+            txtMotion();
+        }
+    });
+
+    function txtMotion() {
+        // 슬라이드 전환 직후의 텍스트 모션
+        $('#slideWrap .slider li div').animate({ top: 0, opacity: 1 });
+    }
+
+    function autoPager() {
+        // 페이저의 이미지 변경
+        // 페이저 a태그의 active 클래스 모두 제거
+        $('#slideWrap .pager a').removeClass('active');
+        // 현재 슬라이드 번호를 가져와서 current에 저장한다.
+        var current = slider.getCurrentSlide();
+        $('#slideWrap .pager a').eq(current).addClass('active');
+
+        // 슬라이드 전환 직전의 텍스트 모션
+        $('#slideWrap .slider li div').css({ top: 100, opacity: 0 });
+    }
+
+    // 페이저 버튼
+    $('#slideWrap .pager a').click(function (e) {
+        // a태그의 기본이벤트 제거
+        e.preventDefault();
+        let num = $(this).index();
+        slider.goToSlide(num);
+        return false;
+    });
+
+    // 이전 버튼
+    $('#slideWrap #prev').click(function (e) {
+        e.preventDefault();
+        slider.goToPrevSlide();
+        autoPager();
+        return false;
+    });
+    // 이후 버튼
+    $('#slideWrap #next').click(function (e) {
+        e.preventDefault();
+        slider.goToNextSlide();
+        autoPager();
+        return false;
     });
 
     // 풀페이지 레이아웃
