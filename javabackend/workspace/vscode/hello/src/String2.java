a5b599bd4a043fa7a557a1f2d098db8cd16ded69
moonsunho <tkdlqj0411@naver.com>
2023-06-26 17:29:55 +0900

    0626

---
+++ javabackend/workspace/vscode/hello/src/String2.java
@@ -0,0 +1,10 @@
+public class String2 {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "java";
+        String c = "hello";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+    }
+}
