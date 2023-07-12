9a68e2602e78dc68d415d2e627072aa292f678b0
moonsunho <tkdlqj0411@naver.com>
2023-07-12 17:43:07 +0900

    0712

--- javabackend/workspace/src/a0712/Board.java
+++ javabackend/workspace/src/a0712/Board.java
@@ -5,17 +5,35 @@ public class Board {
     private String content;
     private String writer;
 
-    public Board(String subject, String content, String writer){
+    public Board(String subject, String content, String writer) {
         this.subject = subject;
         this.content = content;
-        this. writer = writer;
+        this.writer = writer;
+    
     }
 
-    public String getSubject(){return subject;}
-    public void setSubject(){this.subject = subject;}
-    public String getContent(){return content;}
-    public void setContent(){this.content = content;}
-    public String getwWiter(){return writer;}
-    public void setwWiter(){this.writer = writer;}
-    
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject() {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent() {
+        this.content = content;
+    }
+
+    public String getwWiter() {
+        return writer;
+    }
+
+    public void setwWiter() {
+        this.writer = writer;
+    }
+
 }
